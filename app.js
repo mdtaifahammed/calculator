2ba0d2391717beeeb276a1484edb75e22f7a0413
mdtaifahammed <mdtaifahammed18@gmail.com>
2026-04-07 11:11:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+const screen = document.querySelector(".screen");
+const btn = document.querySelectorAll(".box");
+function appendToScreen (input) {
+    screen.value += input;
+}
+function clearScreen () {
+    screen.value = "";
+}
+function backspace () {
+    screen.value = screen.value.slice(0,-1);
+}
+function calculate () {
+    try {
+        screen.value = eval(screen.value);
+    }
+    catch (error) {
+        screen.value = "Error";
+    }
+}
+=======
+const screen = document.querySelector(".screen");
+const btn = document.querySelectorAll(".box");
+function appendToScreen (input) {
+    screen.value += input;
+}
+function clearScreen () {
+    screen.value = "";
+}
+function backspace () {
+    screen.value = screen.value.slice(0,-1);
+}
+function calculate () {
+    try {
+        screen.value = eval(screen.value);
+    }
+    catch (error) {
+        screen.value = "Error";
+    }
+}
+>>>>>>> c9c76963d423df96653bf19782bdf153e2c056c2
