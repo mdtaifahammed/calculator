08ff0c11e08a32061adae6b734f41c4b6a274246
mdtaifahammed <mdtaifahammed18@gmail.com>
2026-04-07 15:24:31 +0530

    Resolve merge conflict in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const screen = document.querySelector(".screen");
 const btn = document.querySelectorAll(".box");
 function appendToScreen (input) {
@@ -18,24 +18,3 @@ function calculate () {
         screen.value = "Error";
     }
 }
-=======
-const screen = document.querySelector(".screen");
-const btn = document.querySelectorAll(".box");
-function appendToScreen (input) {
-    screen.value += input;
-}
-function clearScreen () {
-    screen.value = "";
-}
-function backspace () {
-    screen.value = screen.value.slice(0,-1);
-}
-function calculate () {
-    try {
-        screen.value = eval(screen.value);
-    }
-    catch (error) {
-        screen.value = "Error";
-    }
-}
->>>>>>> c9c76963d423df96653bf19782bdf153e2c056c2
